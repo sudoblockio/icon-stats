2641efc1599f2aeb41166e641f54d59fd12ccb70
shreyasbhat0 <ks.shreyas0@gmail.com>
2024-10-11 14:11:13 +0530

    fix: capitalise router tags

--- icon_stats/api/v1/router.py
+++ icon_stats/api/v1/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from icon_stats.api.v1.endpoints import exchanges_legacy, token_stats, openapi
 
-api_router = APIRouter(prefix="/statistics",tags=["statistics"])
+api_router = APIRouter(prefix="/statistics",tags=["Statistics"])
 api_router.include_router(token_stats.router)
 api_router.include_router(exchanges_legacy.router)
 api_router.include_router(openapi.router)
